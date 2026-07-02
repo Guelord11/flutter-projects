2df36bde93125772deaf891efe060ce3586884b6
Guelord11 <guelordkanyamanda5@gmail.com>
2026-07-02 14:41:36 +0000

    Initial commit: frivia Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:frivia/pages/game_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Frivia',
+      theme: ThemeData(
+        fontFamily: 'ArchitectsDaughter',
+        scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: GamePage(),
+    );
+  }
+}
